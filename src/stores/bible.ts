e183086689d9ab7d9d0fbca268ee0706a6a1bf7a
Davi Viana <davianasales@outlook.com>
2023-05-06 14:24:40 -0300

    feat(stores/bible): store

---
+++ src/stores/bible.ts
@@ -0,0 +1,34 @@
+import { useBibleData, type BibleBook, type BibleChapterDictionary } from "@/processing/data"
+import { defineStore } from "pinia"
+import { computed, ref } from "vue"
+
+export const useBibleStore = defineStore("bible", () => {
+  const bible = useBibleData()
+
+  const books  = ref<BibleBook[]>([])
+  const chapters = ref<BibleChapterDictionary>({})
+
+  const getChapterByAbbrev = computed(
+    () => (abbrev: string) => chapters.value[abbrev]
+  )
+
+  async function refresh() {
+    const data = await bible.createData()
+
+    books.value = data.books
+    chapters.value = data.chapters
+  }
+
+  return {
+    books,
+    chapters,
+    
+    getChapterByAbbrev,
+
+    refresh,
+
+    $reset() {
+      return void 0
+    },
+  }
+})
