5016178af6b2c8d04dc09f828827b529bb65919f
Davi Viana <davianasales@outlook.com>
2023-05-07 00:24:26 -0300

    feat(TheLink): create component

---
+++ src/components/TheLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    type="button"
+    data-te-ripple-init
+    data-te-ripple-color="light"
+    class="inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-100 hover:opacity-90 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700"
+  >
+    <slot>
+      {{ label }}
+    </slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label?: string
+}>()
+</script>
