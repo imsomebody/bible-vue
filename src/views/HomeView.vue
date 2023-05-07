03b2704b34341f376eaf331fb669d0982e735ea7
Davi Viana <davianasales@outlook.com>
2023-05-07 13:56:33 -0300

    feat(HomeView): use button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@
             </TheLink>
             <TheLink class="text-blue-600"> Contribuir ou Saber Mais </TheLink>
           </span>
-          <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div class="relative mb-4 flex w-full flex-wrap items-stretch gap-1">
             <input
               type="search"
               class="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
@@ -27,23 +27,7 @@
             />
 
             <!--Search icon-->
-            <span
-              class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
-              id="basic-addon2"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
+            <Button icon="pi pi-search" size="small" text />
           </div>
           <p class="text-lg text-color">
             Você pode pesquisar acima ou clicar em Navegar para navegar a
@@ -57,4 +41,5 @@
 
 <script setup lang="ts">
 import TheLink from "@/components/TheLink.vue"
+import Button from "primevue/button"
 </script>
